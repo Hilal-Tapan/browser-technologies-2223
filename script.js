012f6236d4bbafcc85f33a83796af8727fb61064
“Hilal-Tapan“ <hilalt98@hotmail.com>
2023-03-29 14:56:18 +0200

    css

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
+var inputEmail= document.getElementById("email");
+inputEmail.oninput = function(e){
+    localStorage.setItem("email", inputEmail.value);
 }
+
+inputEmail.value = localStorage.getItem("email");
+
+
+
+
+
+
+// server side makkelijk maken met id params -> zo min mogelijk invullen -> gebruiksvriendelijker 
+// je moet dit wel kunnen aanpassen in de input value
+// type dit uit in je readme
