f2c4dba015f21bdc6042540aea70150ae918393a
“Hilal-Tapan“ <hilalt98@hotmail.com>
2023-03-30 13:51:18 +0200

    code forloop door alle inputs

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // PERSOONLIJKE INFO
-var inputEmail = document.getElementById("email");
 var inputFirstName = document.getElementById("firstName");
 var inputLastName = document.getElementById("lastName");
 var inputStudentNumber = document.getElementById("studentNumber");
@@ -10,7 +9,7 @@ if (inputFirstName) {
     inputFirstName.oninput = function (e) {
         localStorage.setItem("firstName", inputFirstName.value);
     }
-    // inputFirstName.value = localStorage.getItem("firstName");
+    inputFirstName.value = localStorage.getItem("firstName");
 }
 
 if (inputLastName) {
@@ -37,79 +36,83 @@ if (inputEmail) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // VAK 1
-var inputWafsVraag1 = document.querySelectorAll('[name="wafs-vraag-1"],[name="wafs-vraag-2"]');
 
-if (inputWafsVraag1) {
-    // Loop through all of the selected input elements
-    for (var i = 0; i < inputWafsVraag1.length; i++) {
-        // var definieerd een variabele voor een hele functie
-        // let definieerd een variabele voor een code block {}
-        // dat is wat we hier willen want input elements blijft dus bestaan.
-        // de var bleef bestaan en de let kijkt naar de meest recente waarde.
+// Select all fieldset elements with class "radio"
+let fieldsets = document.querySelectorAll('fieldset.radio');
+// Iterate over each fieldset element
+for (let i = 0; i < fieldsets.length; i++) {
+    // Select all input elements within the current fieldset
+    let inputs = fieldsets[i].querySelectorAll('input[type="radio"]');
 
-        // heeft deze radiobutton de value die ik ken uit local storage
-        // if true -> inputElement.checked = true
-        // 
-        let inputElement = inputWafsVraag1[i];
-        inputElement.oninput = function (e) {
-            localStorage.setItem(inputElement.name, inputElement.value);
+    // Iterate over each input element
+    for (let j = 0; j < inputs.length; j++) {
+        // Store the current input element in inputElement     
+        let inputElement = inputs[j];
+
+        // Get the stored value for this input element's name attribute
+        let storedValue = localStorage.getItem(inputElement.name);
+
+        // Check if a stored value exists for this input element's name attribute
+        if (storedValue !== null) {
+
+            // If the input element radio button,
+            // set its checked property to true if its value attribute matches the stored value
+            if (inputElement.type === "radio") {
+                inputElement.checked = (inputElement.value === storedValue);
+            }
+            // If the input element is not a checkbox or radio button,
+            // set its value property to the stored value
+            else {
+                inputElement.value = storedValue;
+            }
         }
-        // inputElement.value = localStorage.getItem(inputElement.name);
-        console.log(inputElement.value)
-        //if checked is true
-        // en ff kijken of oninput werkt op radiobuttons
+        // Set up an oninput event handler for the input element
+        inputElement.oninput = function (e) {
+            // Save the input element's current value to localStorage
+            localStorage.setItem(inputElement.name, inputElement.value);
+        };
+
+        // Log the current value of the input element to the console
+        console.log(inputElement.value);
     }
 }
 
-var inputWafsVraag1 = document.querySelectorAll('[name="wafs-vraag-1"],[name="wafs-vraag-2"]');
-
-if (inputWafsVraag1) {
-  for (var i = 0; i < inputWafsVraag1.length; i++) {
-    let inputElement = inputWafsVraag1[i];
-    let storedValue = localStorage.getItem(inputElement.name);
-    if (storedValue !== null) {
-      if (inputElement.type === "checkbox" || inputElement.type === "radio") {
-        inputElement.checked = (inputElement.value === storedValue);
-      } else {
-        inputElement.value = storedValue;
-      }
-    }
-    inputElement.oninput = function (e) {
-      localStorage.setItem(inputElement.name, inputElement.value);
-    };
-    console.log(inputElement.value);
-  }
-}
 
 
 
-
+// code zonder for loop door alle inputs
 // var inputWafsVraag1 = document.querySelectorAll('[name="wafs-vraag-1"],[name="wafs-vraag-2"]');
-
 // if (inputWafsVraag1) {
+//     // Loop through all of the selected input elements
 //     for (var i = 0; i < inputWafsVraag1.length; i++) {
-
 //         let inputElement = inputWafsVraag1[i];
-//         inputElement.oninput = function (e) {
-//             localStorage.setItem(inputElement.name, inputElement.value);
-//         }
-//         console.log(inputElement.value)
+//         // var definieerd een variabele voor een hele functie
+//         // let definieerd een variabele voor een code block {}
+//         // dat is wat we hier willen want input elements blijft dus bestaan.
+//         // de var bleef bestaan en de let kijkt naar de meest recente waarde.
 
+//     // Get the stored value for this input element's name attribute
+//     let storedValue = localStorage.getItem(inputElement.name);
+//     // Check if a stored value exists for this input element's name attribute
+//     if (storedValue !== null) {
+//       // If the input element is radio button,
+//       // set its checked property to true if its value attribute matches the stored value
+//       if (inputElement.type === "radio") {
+//         inputElement.checked = (inputElement.value === storedValue);
+//       }
+//       // If the input element is not a radio button,
+//       // set its value property to the stored value
+//       else {
+//         inputElement.value = storedValue;
+//       }
 //     }
+//     // Set up an oninput event handler for the input element
+//     inputElement.oninput = function (e) {
+//       // Save the input element's current value to localStorage
+//       localStorage.setItem(inputElement.name, inputElement.value);
+//     };
+//   }
 // }
-
-// var inputWafsVraag1 = document.querySelectorAll('[name="wafs-vraag-1"]');
-// console.log(inputWafsVraag1);
-
-// // functies store the items
-// if (inputWafsVraag1) {
-//     inputWafsVraag1.oninput = function (e) {
-//         localStorage.setItem("wafs-vraag-1", inputWafsVraag1.value);
-//     }
-//     inputWafsVraag1.value = localStorage.getItem("wafs-vraag-1");
-// }
-
-
 
 
 
